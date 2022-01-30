16553d9a11dacfb22eca84ebad94fd8b875fd426
Fredrik Lind <lindfredrik411@gmail.com>
2022-01-30 22:40:28 +0100

    commit 1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+  import dataFetch from './components/dataFetch.vue'
+
+  export default {
+    components: {
+      dataFetch
+    }
+  }
+</script>
+
+<template>
+  <dataFetch />
+</template>
