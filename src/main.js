3e69fa0bfe0b172ef4b634c3df2c9687d2ab4100
Fredrik Lind <lindfredrik411@gmail.com>
2022-02-02 13:41:44 +0100

    Final

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
+import router from './router'
+import { registerSW } from 'virtual:pwa-register'
 
-createApp(App).mount('#app')
+registerSW();
+
+createApp(App).use(router).mount('#app')
